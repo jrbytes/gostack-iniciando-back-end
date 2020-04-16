8e0874047340794738b19a8ce682be5b93c82fb6
Jr Bytes <itamazor@gmail.com>
2020-04-16 00:39:51 -0300

    Adicionado funcionalidades de autenticação com JWT e upload do avatar

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '6be388cc819e5614019f9d99c5bed43e',
+    expiresIn: '1d',
+  },
+}
--- src/database/migrations/1586912422533-CreateAppointments.ts
+++ src/database/migrations/1586912422533-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1586912422533
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
