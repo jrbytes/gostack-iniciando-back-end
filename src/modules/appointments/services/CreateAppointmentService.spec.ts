a69b9f8eb47620fd9cca6409a727967d1b39c5d8
Jr Bytes <itamazor@gmail.com>
2020-05-12 22:04:49 -0300

    Incremented notifications with mongodb

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,15 +1,22 @@
 import AppError from '@shared/errors/AppError'
 
+import FakeNotificationsRepository from '@modules/notifications/repositories/fakes/FakeNotificationsRepository'
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository'
 import CreateAppointmentService from './CreateAppointmentService'
 
 let fakeAppointmentsRepository: FakeAppointmentsRepository
+let fakeNotificationsRepository: FakeNotificationsRepository
 let createAppointment: CreateAppointmentService
 
 describe('CreateAppointment', () => {
   beforeEach(() => {
     fakeAppointmentsRepository = new FakeAppointmentsRepository()
-    createAppointment = new CreateAppointmentService(fakeAppointmentsRepository)
+    fakeNotificationsRepository = new FakeNotificationsRepository()
+
+    createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+      fakeNotificationsRepository,
+    )
   })
 
   it('should be able to create a new appointment', async () => {
@@ -28,9 +35,7 @@ describe('CreateAppointment', () => {
   })
 
   it('should not be able to create two appointments on the same time', async () => {
-    // conflitou com teste q n aceita appointment anterior ao now
-    // const appointmentDate = new Date(2020, 4, 10, 11)
-    const appointmentDate = new Date(2020, 4, 11, 11)
+    const appointmentDate = new Date(2020, 4, 20, 11)
 
     await createAppointment.execute({
       date: appointmentDate,
