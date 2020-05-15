2da34930ef3ae086960364256cc3f7b714a19e96
Jr Bytes <itamazor@gmail.com>
2020-05-15 01:07:25 -0300

    Incremented cache and middleware rate limit.

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express'
-import { parseISO } from 'date-fns'
 import { container } from 'tsyringe'
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService'
@@ -7,17 +6,14 @@ import CreateAppointmentService from '@modules/appointments/services/CreateAppoi
 export default class AppointmentsControler {
   public async create(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id
-
     const { provider_id, date } = request.body
-
-    const parsedDate = parseISO(date)
 
     const createAppointment = container.resolve(CreateAppointmentService)
 
     const appointment = await createAppointment.execute({
+      date,
       provider_id,
       user_id,
-      date: parsedDate,
     })
 
     return response.json(appointment)
